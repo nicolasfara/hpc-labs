56d8f569a474769e1cc9c516106c5257102c1b45
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2018-11-14 16:43:59 +0100

    fix mandelbrot

--- ex2-mpi/mpi-mandelbrot.c
+++ ex2-mpi/mpi-mandelbrot.c
@@ -6,13 +6,13 @@
  * Written in 2017 by Moreno Marzolla <moreno.marzolla(at)unibo.it>
  * Last updated in 2018 by Moreno Marzolla
  *
- * To the extent possible under law, the author(s) have dedicated all 
- * copyright and related and neighboring rights to this software to the 
+ * To the extent possible under law, the author(s) have dedicated all
+ * copyright and related and neighboring rights to this software to the
  * public domain worldwide. This software is distributed without any warranty.
  *
  * You should have received a copy of the CC0 Public Domain Dedication
- * along with this software. If not, see 
- * <http://creativecommons.org/publicdomain/zero/1.0/>. 
+ * along with this software. If not, see
+ * <http://creativecommons.org/publicdomain/zero/1.0/>.
  *
  * --------------------------------------------------------------------------
  *
@@ -33,29 +33,29 @@
 const int MAXIT = 100;
 
 typedef struct {
-    uint8_t r;  /* red   */
-    uint8_t g;  /* green */
-    uint8_t b;  /* blue  */
+  uint8_t r;  /* red   */
+  uint8_t g;  /* green */
+  uint8_t b;  /* blue  */
 } pixel_t;
 
 /* color gradient from https://stackoverflow.com/questions/16500656/which-color-gradient-is-used-to-color-mandelbrot-in-wikipedia */
 const int colors[][3] = {
-    {66, 30, 15}, /* r, g, b */
-    {25, 7, 26},
-    {9, 1, 47},
-    {4, 4, 73},
-    {0, 7, 100},
-    {12, 44, 138},    
-    {24, 82, 177},
-    {57, 125, 209},
-    {134, 181, 229},
-    {211, 236, 248},
-    {241, 233, 191},
-    {248, 201, 95},
-    {255, 170, 0},
-    {204, 128, 0},
-    {153, 87, 0},
-    {106, 52, 3} };
+  {66, 30, 15}, /* r, g, b */
+  {25, 7, 26},
+  {9, 1, 47},
+  {4, 4, 73},
+  {0, 7, 100},
+  {12, 44, 138},
+  {24, 82, 177},
+  {57, 125, 209},
+  {134, 181, 229},
+  {211, 236, 248},
+  {241, 233, 191},
+  {248, 201, 95},
+  {255, 170, 0},
+  {204, 128, 0},
+  {153, 87, 0},
+  {106, 52, 3} };
 const int NCOLORS = sizeof(colors)/sizeof(colors[0]);
 
 /*
@@ -69,15 +69,15 @@ const int NCOLORS = sizeof(colors)/sizeof(colors[0]);
  */
 int iterate( float cx, float cy )
 {
-    float x = 0.0f, y = 0.0f, xnew, ynew;
-    int it;
-    for ( it = 0; (it < MAXIT) && (x*x + y*y <= 2.0*2.0); it++ ) {
-        xnew = x*x - y*y + cx;
-        ynew = 2.0*x*y + cy;
-        x = xnew;
-        y = ynew;
-    }
-    return it;
+  float x = 0.0f, y = 0.0f, xnew, ynew;
+  int it;
+  for ( it = 0; (it < MAXIT) && (x*x + y*y <= 2.0*2.0); it++ ) {
+    xnew = x*x - y*y + cx;
+    ynew = 2.0*x*y + cy;
+    x = xnew;
+    y = ynew;
+  }
+  return it;
 }
 
 /* Draw the rows of the Mandelbrot set from |ystart| (inclusive) to
@@ -87,68 +87,95 @@ int iterate( float cx, float cy )
    pixels p[0], p[1], ... */
 void draw_lines( int ystart, int yend, pixel_t* p, int xsize, int ysize )
 {
-    int x, y;
-    for ( y = ystart; y < yend; y++) {
-        for ( x = 0; x < xsize; x++ ) {
-            const float cx = -2.5 + 3.5 * (float)x / (xsize - 1);
-            const float cy = 1 - 2.0 * (float)y / (ysize - 1);
-            const int v = iterate(cx, cy);
-            if (v < MAXIT) {
-                p->r = colors[v % NCOLORS][0];
-                p->g = colors[v % NCOLORS][1];
-                p->b = colors[v % NCOLORS][2];
-            } else {
-                p->r = p->g = p->b = 0;
-            }
-            p++;
-        }
+  int x, y;
+  for ( y = ystart; y < yend; y++) {
+    for ( x = 0; x < xsize; x++ ) {
+      const float cx = -2.5 + 3.5 * (float)x / (xsize - 1);
+      const float cy = 1 - 2.0 * (float)y / (ysize - 1);
+      const int v = iterate(cx, cy);
+      if (v < MAXIT) {
+        p->r = colors[v % NCOLORS][0];
+        p->g = colors[v % NCOLORS][1];
+        p->b = colors[v % NCOLORS][2];
+      } else {
+        p->r = p->g = p->b = 0;
+      }
+      p++;
     }
+  }
 }
-    
+
 int main( int argc, char *argv[] )
 {
-    int my_rank, comm_sz;
-    FILE *out = NULL;
-    const char* fname="mandelbrot.ppm";
-    pixel_t *bitmap = NULL;
-    int xsize, ysize;
+  int my_rank, comm_sz;
+  FILE *out = NULL;
+  const char* fname="mandelbrotMPI.ppm";
+  pixel_t *bitmap = NULL;
+  int xsize, ysize;
 
-    MPI_Init(&argc, &argv);
-    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);
+  MPI_Init(&argc, &argv);
+  MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);
 
-    if ( argc > 1 ) {
-        ysize = atoi(argv[1]);
-    } else {
-        ysize = 1024;
+  if ( argc > 1 ) {
+    ysize = atoi(argv[1]);
+  } else {
+    ysize = 1024;
+  }
+
+  xsize = ysize * 1.4;
+
+  if ( 0 == my_rank ) {
+    out = fopen(fname, "w");
+    if ( !out ) {
+      fprintf(stderr, "Error: cannot create %s\n", fname);
+      MPI_Abort(MPI_COMM_WORLD, -1);
     }
 
-    xsize = ysize * 1.4;
-    
-    if ( 0 == my_rank ) {        
-        out = fopen(fname, "w");
-        if ( !out ) {
-            fprintf(stderr, "Error: cannot create %s\n", fname);
-            MPI_Abort(MPI_COMM_WORLD, -1);
-        }
+    /* Write the header of the output file */
+    fprintf(out, "P6\n");
+    fprintf(out, "%d %d\n", xsize, ysize);
+    fprintf(out, "255\n");
 
-        /* Write the header of the output file */
-        fprintf(out, "P6\n");
-        fprintf(out, "%d %d\n", xsize, ysize);
-        fprintf(out, "255\n");
+    /* Allocate the complete bitmap */
+    bitmap = (pixel_t*)malloc(xsize*ysize*sizeof(*bitmap));
+  }
 
-        /* Allocate the complete bitmap */
-        bitmap = (pixel_t*)malloc(xsize*ysize*sizeof(*bitmap));
-        /* [TODO] This is not a true parallel version, since the master
-           does everything */
-        draw_lines(0, ysize, bitmap, xsize, ysize);
-        fwrite(bitmap, sizeof(*bitmap), xsize*ysize, out);
-        fclose(out);
-        free(bitmap);
-    }
+  const int start = ysize * my_rank / comm_sz;
+  const int end = ysize * (my_rank + 1) / comm_sz;
+  const int size = end - start;
+  const int send_size = size * xsize * sizeof(pixel_t);
+  pixel_t *local_bitmap = (pixel_t *) malloc(xsize * size * sizeof(pixel_t));
 
+  int *displs = (int*) malloc(sizeof(int) * comm_sz);
+  int *recvcounts = (int*) malloc(sizeof(int) * comm_sz);
 
-    MPI_Finalize();
+  for (int i = 0; i < comm_sz; i++) {
+    const int my_start = ysize * i / comm_sz;
+    const int my_end = ysize * (i + 1) / comm_sz;
+    const int my_size = my_end - my_start;
+    const int my_send_size = my_size * xsize * sizeof(pixel_t);
+    displs[i] = my_start * xsize * sizeof(pixel_t);
+    recvcounts[i] = my_send_size;
+  }
 
-    return 0;
+  draw_lines(start, end, local_bitmap, xsize, ysize);
+
+  MPI_Gatherv(local_bitmap, send_size, MPI_BYTE, bitmap, recvcounts, displs, MPI_BYTE, 0, MPI_COMM_WORLD);
+
+  if(0 == my_rank) {
+    fwrite(bitmap, sizeof(*bitmap), xsize*ysize, out);
+    fclose(out);
+    free(bitmap);
+  }
+
+  free(local_bitmap);
+  free(recvcounts);
+  free(displs);
+
+  MPI_Finalize();
+
+  return 0;
 }
+
+// vim: set nofoldenable :
