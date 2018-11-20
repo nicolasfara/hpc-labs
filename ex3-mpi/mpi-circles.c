b545ab8346c3b8402baa6b11e50902a2a77173f6
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2018-11-20 12:14:59 +0100

    circle

--- ex3-mpi/mpi-circles.c
+++ ex3-mpi/mpi-circles.c
@@ -31,83 +31,100 @@
 
 float sq(float x)
 {
-    return x*x;
+  return x*x;
 }
 
 /* Generate |k| random points inside the square (0,0) --
-  (100,100). Return the number of points that fall inside at least one
-  of the |n| circles with center (x[i], y[i]) and radius r[i].  The
-  result must be <= |k|. */
+   (100,100). Return the number of points that fall inside at least one
+   of the |n| circles with center (x[i], y[i]) and radius r[i].  The
+   result must be <= |k|. */
 int inside( const float* x, const float* y, const float *r, int n, int k )
 {
-    int i, np, c=0;
-    for (np=0; np<k; np++) {
-        const float px = 100.0*rand()/(float)RAND_MAX;
-        const float py = 100.0*rand()/(float)RAND_MAX;
-        for (i=0; i<n; i++) {
-            if ( sq(px-x[i]) + sq(py-y[i]) <= sq(r[i]) ) {
-                c++;
-                break;
-            }
-        }
+  int i, np, c=0;
+  for (np=0; np<k; np++) {
+    const float px = 100.0*rand()/(float)RAND_MAX;
+    const float py = 100.0*rand()/(float)RAND_MAX;
+    for (i=0; i<n; i++) {
+      if ( sq(px-x[i]) + sq(py-y[i]) <= sq(r[i]) ) {
+        c++;
+        break;
+      }
     }
-    return c;    
+  }
+  return c;
 }
 
 int main( int argc, char* argv[] )
 {
-    float *x = NULL, *y = NULL, *r = NULL;
-    int N, K, c = 0;
-    int my_rank, comm_sz;
+  float *x = NULL, *y = NULL, *r = NULL;
+  int N, K, c = 0;
+  int my_rank, comm_sz;
 
-    MPI_Init(&argc, &argv);
-    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);
+  MPI_Init(&argc, &argv);
+  MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);
 
-    /* Initialize the Random Number Generator (RNG) */
-    srand(time(NULL));
+  /* Initialize the Random Number Generator (RNG) */
+  srand(time(NULL));
 
-    if ( (0 == my_rank) && (argc != 3) ) {
-        fprintf(stderr, "Usage: %s [npoints] [inputfile]\n", argv[0]);
-        MPI_Abort(MPI_COMM_WORLD, EXIT_FAILURE);
+  if ( (0 == my_rank) && (argc != 3) ) {
+    fprintf(stderr, "Usage: %s [npoints] [inputfile]\n", argv[0]);
+    MPI_Abort(MPI_COMM_WORLD, EXIT_FAILURE);
+  }
+
+  K = atoi(argv[1]);
+
+  /* It is required that the input file is read by the master only */
+  if ( 0 == my_rank ) {
+    FILE *in = fopen(argv[2], "r");
+    int i;
+    if ( !in ) {
+      fprintf(stderr, "Cannot open %s for reading\n", argv[2]);
+      MPI_Abort(MPI_COMM_WORLD, EXIT_FAILURE);
     }
-
-    K = atoi(argv[1]);
-
-    /* It is required that the input file is read by the master only */
-    if ( 0 == my_rank ) {
-        FILE *in = fopen(argv[2], "r");
-        int i;
-        if ( !in ) {
-            fprintf(stderr, "Cannot open %s for reading\n", argv[1]);
-            MPI_Abort(MPI_COMM_WORLD, EXIT_FAILURE);
-        }
-        fscanf(in, "%d", &N);
-        x = (float*)malloc(N * sizeof(*x));
-        y = (float*)malloc(N * sizeof(*y));
-        r = (float*)malloc(N * sizeof(*r));
-        for (i=0; i<N; i++) {
-            fscanf(in, "%f %f %f", &x[i], &y[i], &r[i]);
-        }
-        fclose(in);
+    fscanf(in, "%d", &N);
+    x = (float*)malloc(N * sizeof(*x));
+    y = (float*)malloc(N * sizeof(*y));
+    r = (float*)malloc(N * sizeof(*r));
+    for (i=0; i<N; i++) {
+      fscanf(in, "%f %f %f", &x[i], &y[i], &r[i]);
     }
+    fclose(in);
+  }
 
-    /* [TODO] This is not a true parallel version: the master does
-       everything */
-    if ( 0 == my_rank ) {
-        c = inside(x, y, r, N, K);
-    }
-    
-    /* the master prints the area */
-    if ( 0 == my_rank ) {
-        printf("%d points, %d inside, area %f\n", K, c, 1.0e6*c/K);
-    }
+  MPI_Bcast(&N, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
-    free(x);
-    free(y);
-    free(r);
+  const int start = my_rank * K / comm_sz;
+  const int end = (my_rank + 1) * K / comm_sz;
+  const int size = end - start;
 
-    MPI_Finalize();
+  if(my_rank != 0) {
+    x = (float*) malloc(N * sizeof(*x));
+    y = (float*) malloc(N * sizeof(*y));
+    r = (float*) malloc(N * sizeof(*r));
+  }
 
-    return EXIT_SUCCESS;
+  MPI_Bcast(x, N, MPI_FLOAT, 0, MPI_COMM_WORLD);
+  MPI_Bcast(y, N, MPI_FLOAT, 0, MPI_COMM_WORLD);
+  MPI_Bcast(r, N, MPI_FLOAT, 0, MPI_COMM_WORLD);
+
+  int local_c = inside(x, y, r, N, size);
+
+  MPI_Reduce(&local_c, &c, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+
+  /* the master prints the area */
+  if ( 0 == my_rank ) {
+    printf("%d points, %d inside, area %f\n", K, c, 1.0e6*c/K);
+  }
+
+  free(x);
+  free(y);
+  free(r);
+
+  MPI_Finalize();
+
+  return EXIT_SUCCESS;
 }
+
+
+// vim: set nofoldenable :
--- ex3-mpi/mpi-rule30.c
+++ ex3-mpi/mpi-rule30.c
@@ -46,16 +46,16 @@ const int HALO = 1;
  */
 void step( const cell_t *cur, cell_t *next, int ext_n )
 {
-    int i;
-    for (i=HALO; i<ext_n-HALO; i++) {
-        const cell_t east = cur[i-1];
-        const cell_t center = cur[i];
-        const cell_t west = cur[i+1];
-        next[i] = ( (east && !center && !west) ||
-                    (!east && !center && west) ||
-                    (!east && center && !west) ||
-                    (!east && center && west) );
-    }
+  int i;
+  for (i=HALO; i<ext_n-HALO; i++) {
+    const cell_t east = cur[i-1];
+    const cell_t center = cur[i];
+    const cell_t west = cur[i+1];
+    next[i] = ( (east && !center && !west) ||
+        (!east && !center && west) ||
+        (!east && center && !west) ||
+        (!east && center && west) );
+  }
 }
 
 /**
@@ -65,11 +65,11 @@ void step( const cell_t *cur, cell_t *next, int ext_n )
  */
 void init_domain( cell_t *cur, int ext_n )
 {
-    int i;
-    for (i=0; i<ext_n; i++) {
-        cur[i] = 0;
-    }
-    cur[ext_n/2] = 1;
+  int i;
+  for (i=0; i<ext_n; i++) {
+    cur[i] = 0;
+  }
+  cur[ext_n/2] = 1;
 }
 
 /**
@@ -78,200 +78,138 @@ void init_domain( cell_t *cur, int ext_n )
  */
 void dump_state( FILE *out, const cell_t *cur, int ext_n )
 {
-    int i;
-    for (i=HALO; i<ext_n-HALO; i++) {
-        fprintf(out, "%d ", cur[i]);
-    }
-    fprintf(out, "\n");
+  int i;
+  for (i=HALO; i<ext_n-HALO; i++) {
+    fprintf(out, "%d ", cur[i]);
+  }
+  fprintf(out, "\n");
 }
 
 int main( int argc, char* argv[] )
 {
-    const char *outname = "rule30.pbm";
-    FILE *out = NULL;
-    int width, ext_width, steps = 1024, s;        
-    /* |cur| is the memory buffer containint |width| elements; this is
-       the full state of the CA. */
-    cell_t *cur = NULL, *next = NULL, *tmp;
-    int my_rank, comm_sz;
+  const char *outname = "rule30.pbm";
+  FILE *out = NULL;
+  int width, ext_width, steps = 1024, s;        
+  /* |cur| is the memory buffer containint |width| elements; this is
+     the full state of the CA. */
+  cell_t *cur = NULL, *next = NULL, *tmp;
+  int my_rank, comm_sz;
 
-    MPI_Init(&argc, &argv);
-    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);
+  MPI_Init(&argc, &argv);
+  MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);
 
-    if ( 0 == my_rank && argc > 3 ) {
-        fprintf(stderr, "Usage: %s [width [steps]]\n", argv[0]);
-        MPI_Abort(MPI_COMM_WORLD, EXIT_FAILURE);
+  if ( 0 == my_rank && argc > 3 ) {
+    fprintf(stderr, "Usage: %s [width [steps]]\n", argv[0]);
+    MPI_Abort(MPI_COMM_WORLD, EXIT_FAILURE);
+  }
+
+  if ( argc > 1 ) {
+    width = atoi(argv[1]);
+  } else {
+    width = comm_sz * 256;
+  }
+
+  if ( argc > 2 ) {
+    steps = atoi(argv[2]);
+  }
+
+  if ( (0 == my_rank) && (width % comm_sz) ) {
+    printf("The image width (%d) must be a multiple of comm_sz (%d)\n", width, comm_sz);
+    MPI_Abort(MPI_COMM_WORLD, EXIT_FAILURE);
+  }
+
+  /* ext_width is the width PLUS the halo */    
+  ext_width = width + 2*HALO;
+
+  /* The master creates the output file */    
+  if ( 0 == my_rank ) {
+    out = fopen(outname, "w");
+    if ( !out ) {
+      fprintf(stderr, "Cannot create %s\n", outname);
+      MPI_Abort(MPI_COMM_WORLD, EXIT_FAILURE);
     }
-    
-    if ( argc > 1 ) {
-        width = atoi(argv[1]);
-    } else {
-        width = comm_sz * 256;
-    }
+    fprintf(out, "P1\n");
+    fprintf(out, "# produced by %s %d %d\n", argv[0], width, steps);
+    fprintf(out, "%d %d\n", width, steps);
 
-    if ( argc > 2 ) {
-        steps = atoi(argv[2]);
-    }
+    /* Initialize the domain */
+    cur = (cell_t*)malloc( ext_width * sizeof(*cur) );
+    next = (cell_t*)malloc( ext_width * sizeof(*next) );
+    init_domain(cur, ext_width);
+  }
 
-    if ( (0 == my_rank) && (width % comm_sz) ) {
-        printf("The image width (%d) must be a multiple of comm_sz (%d)\n", width, comm_sz);
-        MPI_Abort(MPI_COMM_WORLD, EXIT_FAILURE);
-    }
+  /* [TODO] The following code already contains some pieces that
+     need to be filled in for the parallel version. */
 
-    /* ext_width is the width PLUS the halo */    
-    ext_width = width + 2*HALO;
-    
-    /* The master creates the output file */    
+  /* [TODO] compute the rank of the next and previous process on the
+     chain. These will be used to exchange the boundary */
+
+  const int rank_next = (my_rank + 1) % comm_sz;
+  const int rank_prev = (my_rank - 1 + comm_sz) % comm_sz;
+
+  /* [TODO] size of each local domain; this will be set to |width /
+     comm_sz + 2*HALO|, since it must include the ghost cells */
+  const int local_width = (width / comm_sz) + 2 * HALO;
+
+  /* [TODO] |local_cur| and |local_next| are the local domains,
+     handled by each MPI process. They both have |local_width|
+     elements each */
+  cell_t *local_cur = (cell_t*) malloc(local_width * sizeof(cell_t));
+  cell_t *local_next = (cell_t*) malloc(local_width * sizeof(cell_t));
+
+  for (s=0; s<steps; s++) {
+
+    /* [TODO] Here you should scatter the |cur| array into
+       |local_cur|; we are assuming that the width of the domain
+       is multiple of comm_sz. */
+    MPI_Scatter(&cur[1], width / comm_sz, MPI_CHAR, &local_cur[1], width / comm_sz, MPI_CHAR, 0, MPI_COMM_WORLD);
+
+    /* This is OK; do not modify it */
     if ( 0 == my_rank ) {
-        out = fopen(outname, "w");
-        if ( !out ) {
-            fprintf(stderr, "Cannot create %s\n", outname);
-            MPI_Abort(MPI_COMM_WORLD, EXIT_FAILURE);
-        }
-        fprintf(out, "P1\n");
-        fprintf(out, "# produced by %s %d %d\n", argv[0], width, steps);
-        fprintf(out, "%d %d\n", width, steps);
-
-        /* Initialize the domain */
-        cur = (cell_t*)malloc( ext_width * sizeof(*cur) );
-        next = (cell_t*)malloc( ext_width * sizeof(*next) );
-        init_domain(cur, ext_width);
+      /* Dump the current state to the output image */
+      dump_state(out, cur, ext_width);
     }
 
-    /* [TODO] The following code already contains some pieces that
-       need to be filled in for the parallel version. */
-    
-    /* [TODO] compute the rank of the next and previous process on the
-       chain. These will be used to exchange the boundary */
-    
-    /*
-    const int rank_next = ...
-    const int rank_prev = ...
-    */
-    
-    /* [TODO] size of each local domain; this will be set to |width /
-       comm_sz + 2*HALO|, since it must include the ghost cells */
-    /*
-    const int local_width = ...
-    */
-    
-    /* [TODO] |local_cur| and |local_next| are the local domains,
-       handled by each MPI process. They both have |local_width|
-       elements each */
-    /*
-    cell_t *local_cur = ...
-    cell_t *local_next = ...
-    */
-    
-    for (s=0; s<steps; s++) {
+    /* [TODO] the following "if" should be modified to allow
+       neighbors to exchange ghost cells; two MPI_Sendrecv
+       operations are required to do so. */
+    MPI_Sendrecv(&local_cur[local_width - 2], 1, MPI_CHAR, rank_next, 0, local_cur, 1, MPI_CHAR, rank_prev, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 
-        /* [TODO] Here you should scatter the |cur| array into
-           |local_cur|; we are assuming that the width of the domain
-           is multiple of comm_sz. */
-        /*
-        MPI_Scatter( sendbuf,
-                     sendcount,
-                     datatype,
-                     recvbuf,
-                     recvcount,
-                     datatype,
-                     root,
-                     MPI_COMM_WORLD
-                     );        
-        */
+    MPI_Sendrecv(&local_cur[HALO], 1, MPI_CHAR, rank_prev, 0, &local_cur[local_width - 1], 1, MPI_CHAR, rank_next, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 
-        /* This is OK; do not modify it */
-        if ( 0 == my_rank ) {
-            /* Dump the current state to the output image */
-            dump_state(out, cur, ext_width);
-        }
+    /* [TODO] replace the following block; each process calls
+       |step()| on its local domain */
+      /* Compute the next state */
+    step(local_cur, local_next, local_width);
 
-        /* [TODO] the following "if" should be modified to allow
-           neighbors to exchange ghost cells; two MPI_Sendrecv
-           operations are required to do so. */
-        /* Receive left boundary from left neighbor; send
-           right boundary to right neighbor;
-        MPI_Sendrecv( sendbuf,
-                      sendcount,
-                      datatype,
-                      dest,
-                      sendtag,
-                      recvbuf,
-                      recvcount,
-                      datatype,
-                      source,
-                      recvtag.
-                      MPI_COMM_WORLD,
-                      MPI_STATUS_IGNORE
-                      );
-        */
+    /* [TODO] here we gather the local domains into the
+       |cur| array; indeed, in the parallel version the
+       master does not need |next| at all */
+    MPI_Gather(&local_next[1], local_width - 2, MPI_CHAR, &cur[1], local_width - 2, MPI_CHAR, 0, MPI_COMM_WORLD);
 
-        /* Receive right boundary from right neighbor; send
-           left boundary to left neighbor;
-        MPI_Sendrecv( sendbuf,
-                      sendcount,
-                      datatype,
-                      dest,
-                      sendtag,
-                      recvbuf,
-                      recvcount,
-                      datatype,
-                      source,
-                      recvtag.
-                      MPI_COMM_WORLD,
-                      MPI_STATUS_IGNORE
-                      );
-        */    
-        if (0 == my_rank) {
-            cur[0] = cur[ext_width-2*HALO];
-            cur[ext_width-HALO] = cur[HALO];
-        }
+    /* [TODO] Replace the following block; each process
+       exchanges local_cur and local_next */
+    /*if (0 == my_rank) {
+      //swap cur and next
+      tmp = cur;
+      cur = next;
+      next = tmp;
+    }*/
+  }
+  free(next);
+  free(local_cur);
+  free(local_next);
 
-        /* [TODO] replace the following block; each process calls
-           |step()| on its local domain */
-        if (0 == my_rank) {
-            /* Compute the next state */
-            step(cur, next, ext_width);
-        }
+  if ( 0 == my_rank ) {
+    fclose(out);
+    free(cur);
+  }
 
-        /* [TODO] here we gather the local domains into the
-           |cur| array; indeed, in the parallel version the
-           master does not need |next| at all */
-        /*
-        MPI_Gather( sendbuf,
-                    sendcount,
-                    datatype,
-                    recvbuf,
-                    recvcount,
-                    datatype,
-                    root,
-                    MPI_COMM_WORLD
-                    );
-        */
+  MPI_Finalize();
 
-        /* [TODO] Replace the following block; each process
-           exchanges local_cur and local_next */
-        if (0 == my_rank) {
-            /* swap cur and next */
-            tmp = cur;
-            cur = next;
-            next = tmp;
-        }
-    }
-    free(next);
-
-    /* 
-    free(local_cur);
-    free(local_next);
-    */
-    
-    if ( 0 == my_rank ) {
-        fclose(out);
-        free(cur);
-    }
-    
-    MPI_Finalize();
-
-    return EXIT_SUCCESS;
+  return EXIT_SUCCESS;
 }
+
+
+// vim: set nofoldenable :
