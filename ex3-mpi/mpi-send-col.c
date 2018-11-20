00f998ff146dad12e2101b2e43992fc331cfc2fe
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2018-11-20 12:23:11 +0100

    formatted code

--- ex3-mpi/mpi-send-col.c
+++ ex3-mpi/mpi-send-col.c
@@ -36,64 +36,66 @@
    which is set to -1. */
 void init_matrix( int *m, int size, int k )
 {
-    int i, j;
-    for (i=0; i<size; i++) {
-        for (j=0; j<size+2; j++) {
-            if ( j==0 || j==size+1) {
-                m[i*(size+2)+j] = -1;
-            } else {
-                m[i*(size+2)+j] = k;
-                k++;
-            }
-        }
+  int i, j;
+  for (i=0; i<size; i++) {
+    for (j=0; j<size+2; j++) {
+      if ( j==0 || j==size+1) {
+        m[i*(size+2)+j] = -1;
+      } else {
+        m[i*(size+2)+j] = k;
+        k++;
+      }
     }
+  }
 }
 
 void print_matrix( int *m, int size )
 {
-    int i, j;
-    for (i=0; i<size; i++) {
-        for (j=0; j<size+2; j++) {
-            printf("%3d ", m[i*(size+2)+j]);
-        }
-        printf("\n");
+  int i, j;
+  for (i=0; i<size; i++) {
+    for (j=0; j<size+2; j++) {
+      printf("%3d ", m[i*(size+2)+j]);
     }
+    printf("\n");
+  }
 }
 
 int main( int argc, char *argv[] )
 {
-    int my_rank, comm_sz;
-    int my_mat[SIZE][SIZE+2];
+  int my_rank, comm_sz;
+  int my_mat[SIZE][SIZE+2];
 
-    MPI_Init( &argc, &argv );
-    MPI_Comm_rank( MPI_COMM_WORLD, &my_rank );
-    MPI_Comm_size( MPI_COMM_WORLD, &comm_sz );
+  MPI_Init( &argc, &argv );
+  MPI_Comm_rank( MPI_COMM_WORLD, &my_rank );
+  MPI_Comm_size( MPI_COMM_WORLD, &comm_sz );
 
-    if ( 0 == my_rank && 2 != comm_sz ) {
-        fprintf(stderr, "You must execute exactly 2 processes\n");
-        MPI_Abort(MPI_COMM_WORLD, EXIT_FAILURE);
-    }
-    if ( 0 == my_rank ) {
-        init_matrix(&my_mat[0][0], SIZE, 0);
-    } else if (1 == my_rank) {
-        init_matrix(&my_mat[0][0], SIZE, SIZE*SIZE);
-    }
+  if ( 0 == my_rank && 2 != comm_sz ) {
+    fprintf(stderr, "You must execute exactly 2 processes\n");
+    MPI_Abort(MPI_COMM_WORLD, EXIT_FAILURE);
+  }
+  if ( 0 == my_rank ) {
+    init_matrix(&my_mat[0][0], SIZE, 0);
+  } else if (1 == my_rank) {
+    init_matrix(&my_mat[0][0], SIZE, SIZE*SIZE);
+  }
 
-    /* [TODO] Exchange borders here */
-    
-    /* Print the matrices after the exchange; to do so without
-       interference we must use this funny strategy: process 0 prints,
-       then the processes synchronize, then process 1 prints. */
-    if ( 0 == my_rank ) {
-        printf("\n\nProcess 0:\n");
-        print_matrix(&my_mat[0][0], SIZE);
-    }
-    MPI_Barrier(MPI_COMM_WORLD);
-    if ( 1 == my_rank ) {
-        printf("\n\nProcess 1:\n");
-        print_matrix(&my_mat[0][0], SIZE);
-    }
+  /* [TODO] Exchange borders here */
 
-    MPI_Finalize();
-    return EXIT_SUCCESS;
+  /* Print the matrices after the exchange; to do so without
+     interference we must use this funny strategy: process 0 prints,
+     then the processes synchronize, then process 1 prints. */
+  if ( 0 == my_rank ) {
+    printf("\n\nProcess 0:\n");
+    print_matrix(&my_mat[0][0], SIZE);
+  }
+  MPI_Barrier(MPI_COMM_WORLD);
+  if ( 1 == my_rank ) {
+    printf("\n\nProcess 1:\n");
+    print_matrix(&my_mat[0][0], SIZE);
+  }
+
+  MPI_Finalize();
+  return EXIT_SUCCESS;
 }
+
+// vim: set nofoldenable :
