425d5b14134517e698ddbd176e46bb3caed510c5
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2018-12-18 11:24:37 +0100

    dot threshold

--- ex1-simd/simd-dot.c
+++ ex1-simd/simd-dot.c
@@ -41,99 +41,115 @@ typedef float v4f __attribute__((vector_size(16)));
 #define VLEN (sizeof(v4f)/sizeof(float))
 
 /* Returns the minimum value of a nonempty array v with n elements */
-float serial_dot(const float *x, const float *y, int n)    
+float serial_dot(const float *x, const float *y, int n)
 {
-    double r = 0.0; /* use double here to avoid some nasty rounding errors */
-    int i;
-    for (i=0; i<n; i++) {
-        r += x[i] * y[i];
-    }
-    return r;
+  double r = 0.0; /* use double here to avoid some nasty rounding errors */
+  int i;
+  for (i=0; i<n; i++) {
+    r += x[i] * y[i];
+  }
+  return r;
 }
 
 /* Same as above, but using the vector datatype of GCC */
 float simd_dot(const float *x, const float *y, int n)
 {
-    /* [TODO] */
-    return 0;
+  v4f *m_x, *m_y;
+  v4f vs = { 0.0f, 0.0f, 0.0f, 0.0f };
+  float sum = 0.0;
+  int i;
+  for (i = 0; i < n - VLEN + 1; i += VLEN) {
+    m_x = (v4f *) (x + i);
+    m_y = (v4f *) (y + i);
+    vs += *m_x * *m_y;
+  }
+
+  sum = vs[0] + vs[1] + vs[2] + vs[3];
+  for (; i < n; i++) {
+    sum += x[i];
+  }
+
+  return sum;
 }
 
 /* Initialize vectors x and y */
 void fill(float* x, float* y, int n)
 {
-    int i;
-    const float xx[] = {-2.0f, 0.0f, 4.0f, 2.0f};
-    const float yy[] = { 1.0f/2.0, 0.0f, 1.0/16.0, 1.0f/2.0f};
+  int i;
+  const float xx[] = {-2.0f, 0.0f, 4.0f, 2.0f};
+  const float yy[] = { 1.0f/2.0, 0.0f, 1.0/16.0, 1.0f/2.0f};
 
-    for (i=0; i<n; i++) {
-        x[i] = xx[i % sizeof(xx)/sizeof(xx[0])];
-        y[i] = yy[i % sizeof(yy)/sizeof(yy[0])];;
-    }
+  for (i=0; i<n; i++) {
+    x[i] = xx[i % sizeof(xx)/sizeof(xx[0])];
+    y[i] = yy[i % sizeof(yy)/sizeof(yy[0])];;
+  }
 }
 
 int main(int argc, char* argv[])
 {
-    const int nruns = 10; /* number of replications */
-    int r, n = 10*1024*1024;
-    double serial_elapsed, simd_elapsed;
-    double tstart, tend;
-    float *x, *y, serial_result, simd_result;
-    int ret;
+  const int nruns = 10; /* number of replications */
+  int r, n = 10*1024*1024;
+  double serial_elapsed, simd_elapsed;
+  double tstart, tend;
+  float *x, *y, serial_result, simd_result;
+  int ret;
 
-    if ( argc > 2 ) {
-        fprintf(stderr, "Usage: %s [n]\n", argv[0]);
-        return EXIT_FAILURE;
-    }
+  if ( argc > 2 ) {
+    fprintf(stderr, "Usage: %s [n]\n", argv[0]);
+    return EXIT_FAILURE;
+  }
 
-    if ( argc > 1 ) {
-        n = atoi(argv[1]);
-    }
+  if ( argc > 1 ) {
+    n = atoi(argv[1]);
+  }
 
-    assert(n > 0);
+  assert(n > 0);
 
-    const size_t size = n * sizeof(*x);
+  const size_t size = n * sizeof(*x);
 
-    assert( size < 1024*1024*200UL );
-    ret = posix_memalign((void**)&x, __BIGGEST_ALIGNMENT__, size); 
-    assert( 0 == ret );
-    ret = posix_memalign((void**)&y, __BIGGEST_ALIGNMENT__, size); 
-    assert( 0 == ret );
+  assert( size < 1024*1024*200UL );
+  ret = posix_memalign((void**)&x, __BIGGEST_ALIGNMENT__, size); 
+  assert( 0 == ret );
+  ret = posix_memalign((void**)&y, __BIGGEST_ALIGNMENT__, size); 
+  assert( 0 == ret );
 
-    printf("Array length = %d\n", n);
-    
-    fill(x, y, n);
-    /* Collect execution time of serial version */
-    serial_elapsed = 0.0;
-    for (r=0; r<nruns; r++) {
-        tstart = hpc_gettime();
-        serial_result = serial_dot(x, y, n);
-        tend = hpc_gettime();
-        serial_elapsed += tend - tstart;
-    }
-    serial_elapsed /= nruns;
+  printf("Array length = %d\n", n);
 
-    fill(x, y, n);
-    /* Collect execution time of the parallel version */
-    simd_elapsed = 0.0;
-    for (r=0; r<nruns; r++) {
-        tstart = hpc_gettime();
-        simd_result = simd_dot(x, y, n);
-        tend = hpc_gettime();
-        simd_elapsed += tend - tstart;
-    }
-    simd_elapsed /= nruns;
+  fill(x, y, n);
+  /* Collect execution time of serial version */
+  serial_elapsed = 0.0;
+  for (r=0; r<nruns; r++) {
+    tstart = hpc_gettime();
+    serial_result = serial_dot(x, y, n);
+    tend = hpc_gettime();
+    serial_elapsed += tend - tstart;
+  }
+  serial_elapsed /= nruns;
 
-    printf("Serial: result=%f, time=%f (%d runs)\n", serial_result, serial_elapsed, nruns);
-    printf("SIMD  : result=%f, time=%f (%d runs)\n", simd_result, simd_elapsed, nruns);
+  fill(x, y, n);
+  /* Collect execution time of the parallel version */
+  simd_elapsed = 0.0;
+  for (r=0; r<nruns; r++) {
+    tstart = hpc_gettime();
+    simd_result = simd_dot(x, y, n);
+    tend = hpc_gettime();
+    simd_elapsed += tend - tstart;
+  }
+  simd_elapsed /= nruns;
 
-    if ( fabs(serial_result - simd_result) > 1e-5 ) {
-        fprintf(stderr, "Check FAILED\n");
-        return EXIT_FAILURE;
-    }
+  printf("Serial: result=%f, time=%f (%d runs)\n", serial_result, serial_elapsed, nruns);
+  printf("SIMD  : result=%f, time=%f (%d runs)\n", simd_result, simd_elapsed, nruns);
 
-    printf("Speedup (serial/simd) %f\n", serial_elapsed / simd_elapsed);
+  if ( fabs(serial_result - simd_result) > 1e-5 ) {
+    fprintf(stderr, "Check FAILED\n");
+    return EXIT_FAILURE;
+  }
 
-    free(x);
-    free(y);
-    return EXIT_SUCCESS;
+  printf("Speedup (serial/simd) %f\n", serial_elapsed / simd_elapsed);
+
+  free(x);
+  free(y);
+  return EXIT_SUCCESS;
 }
+
+// vim: set nofoldenable :
--- ex1-simd/simd-threshold.c
+++ ex1-simd/simd-threshold.c
@@ -40,10 +40,10 @@ typedef unsigned char v16uc __attribute__((vector_size(16)));
 #define VLEN (sizeof(v16uc)/sizeof(unsigned char))
 
 typedef struct {
-    int width;   /* Width of the image (in pixels) */
-    int height;  /* Height of the image (in pixels) */
-    int maxgrey; /* Don't care (used only by the PGM read/write routines) */
-    unsigned char *bmap; /* buffer of width*height bytes; each element represents the gray level of a pixel (0-255) */
+  int width;   /* Width of the image (in pixels) */
+  int height;  /* Height of the image (in pixels) */
+  int maxgrey; /* Don't care (used only by the PGM read/write routines) */
+  unsigned char *bmap; /* buffer of width*height bytes; each element represents the gray level of a pixel (0-255) */
 } img_t;
 
 /* Read a PGM file from file |f|. This function is not very robust; it
@@ -51,41 +51,41 @@ typedef struct {
    cat.pgm file. */
 void read_pgm( FILE *f, img_t* img )
 {
-    char buf[1024];
-    const size_t BUFSIZE = sizeof(buf);
-    char *s; 
-    int nread, ret;
+  char buf[1024];
+  const size_t BUFSIZE = sizeof(buf);
+  char *s; 
+  int nread, ret;
 
-    /* Get the file type (must be "P5") */
+  /* Get the file type (must be "P5") */
+  s = fgets(buf, BUFSIZE, f);
+  if (0 != strcmp(s, "P5\n")) {
+    fprintf(stderr, "Wrong file type %s\n", buf);
+    exit(EXIT_FAILURE);
+  }
+  /* Get any comment and ignore it; does not work if there are
+     leading spaces in the comment line */
+  do {
     s = fgets(buf, BUFSIZE, f);
-    if (0 != strcmp(s, "P5\n")) {
-        fprintf(stderr, "Wrong file type %s\n", buf);
-        exit(EXIT_FAILURE);
-    }
-    /* Get any comment and ignore it; does not work if there are
-       leading spaces in the comment line */
-    do {
-        s = fgets(buf, BUFSIZE, f);
-    } while (s[0] == '#');
-    /* Get width, height */
-    sscanf(s, "%d %d", &(img->width), &(img->height));
-    /* get maxgrey; must be less than or equal to 255 */
-    s = fgets(buf, BUFSIZE, f);
-    sscanf(s, "%d", &(img->maxgrey));
-    if ( img->maxgrey > 255 ) {
-        fprintf(stderr, "Error: maxgray > 255 (%d)\n", img->maxgrey);
-        exit(EXIT_FAILURE);
-    }
-    /* Get the binary data. Note that the pointer img->bmap must be
-       properly aligned to allow SIMD instructions, because the
-       compiler emits SIMD instructions for aligned load/stores only. */
-    ret = posix_memalign((void**)&(img->bmap), __BIGGEST_ALIGNMENT__, (img->width)*(img->height));
-    assert( 0 == ret );
-    nread = fread(img->bmap, 1, (img->width)*(img->height), f);
-    if ( (img->width)*(img->height) != nread ) {
-        fprintf(stderr, "Error reading input file: expecting %d bytes, got %d\n", (img->width)*(img->height), nread);
-        exit(EXIT_FAILURE);
-    }
+  } while (s[0] == '#');
+  /* Get width, height */
+  sscanf(s, "%d %d", &(img->width), &(img->height));
+  /* get maxgrey; must be less than or equal to 255 */
+  s = fgets(buf, BUFSIZE, f);
+  sscanf(s, "%d", &(img->maxgrey));
+  if ( img->maxgrey > 255 ) {
+    fprintf(stderr, "Error: maxgray > 255 (%d)\n", img->maxgrey);
+    exit(EXIT_FAILURE);
+  }
+  /* Get the binary data. Note that the pointer img->bmap must be
+     properly aligned to allow SIMD instructions, because the
+     compiler emits SIMD instructions for aligned load/stores only. */
+  ret = posix_memalign((void**)&(img->bmap), __BIGGEST_ALIGNMENT__, (img->width)*(img->height));
+  assert( 0 == ret );
+  nread = fread(img->bmap, 1, (img->width)*(img->height), f);
+  if ( (img->width)*(img->height) != nread ) {
+    fprintf(stderr, "Error reading input file: expecting %d bytes, got %d\n", (img->width)*(img->height), nread);
+    exit(EXIT_FAILURE);
+  }
 }
 
 /**
@@ -93,11 +93,11 @@ void read_pgm( FILE *f, img_t* img )
  */
 void write_pgm( FILE *f, const img_t* img )
 {
-    fprintf(f, "P5\n");
-    fprintf(f, "# produced by cat-map\n");
-    fprintf(f, "%d %d\n", img->width, img->height);
-    fprintf(f, "%d\n", img->maxgrey);
-    fwrite(img->bmap, 1, (img->width)*(img->height), f);
+  fprintf(f, "P5\n");
+  fprintf(f, "# produced by cat-map\n");
+  fprintf(f, "%d %d\n", img->width, img->height);
+  fprintf(f, "%d\n", img->maxgrey);
+  fwrite(img->bmap, 1, (img->width)*(img->height), f);
 }
 
 /**
@@ -105,9 +105,9 @@ void write_pgm( FILE *f, const img_t* img )
  */
 void free_pgm( img_t *img )
 {
-    img->width = img->height = img->maxgrey = -1;
-    free(img->bmap);
-    img->bmap = NULL;
+  img->width = img->height = img->maxgrey = -1;
+  free(img->bmap);
+  img->bmap = NULL;
 }
 
 /*
@@ -116,48 +116,56 @@ void free_pgm( img_t *img )
  */
 void threshold( img_t* img, unsigned char thr )
 {
-    const int width = img->width;
-    const int height = img->height;
-    unsigned char *bmap = img->bmap;
-    int i, j;
+  const int width = img->width;
+  const int height = img->height;
+  unsigned char *bmap = img->bmap;
+  int i, j;
 
-    for (i=0; i<height; i++) {
-        for (j=0; j<width; j++) {
-            unsigned char *pixel = bmap + i*width + j;
-            if (*pixel <= thr ) {
-                *pixel = 0;
-            } else {
-                *pixel = 255;
-            }
-        }
+  const v16uc black = {255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255};
+  const v16uc white = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+
+  for (i=0; i<height; i++) {
+    for (j=0; j<width - VLEN + 1; j += VLEN) {
+      v16uc *pixel = (v16uc *) (bmap + i*width + j);
+      const v16uc mask = (*pixel <= thr);
+      *pixel = (mask & white) | (~mask & black);
+      // Optimization: *pixel = (~mask & black);
+      /*if (*pixel <= thr ) {
+        *pixel = 0;
+      } else {
+        *pixel = 255;
+      }*/
     }
+  }
 }
 
 int main( int argc, char* argv[] )
 {
-    img_t bmap;
-    int thr;
-    double tstart, elapsed;
-    
-    if ( argc != 2 ) {
-        fprintf(stderr, "Usage: %s thr < in.pgm > out.pgm\n", argv[0]);
-        return EXIT_FAILURE;
-    }
-    thr = atoi(argv[1]);
-    if (thr < 0 || thr >= 255) {
-        fprintf(stderr, "FATAL: invalid threshold %d\n", thr);
-        return EXIT_FAILURE;
-    }
-    read_pgm(stdin, &bmap);
-    if ( bmap.width % VLEN ) {
-        fprintf(stderr, "FATAL: the image width (%d) must be multiple of %d\n", bmap.width, (int)VLEN);
-        return EXIT_FAILURE;
-    }
-    tstart = hpc_gettime();
-    threshold(&bmap, thr);
-    elapsed = hpc_gettime() - tstart;
-    fprintf(stderr, "Executon time: %f\n", elapsed);
-    write_pgm(stdout, &bmap);
-    free_pgm(&bmap);
-    return EXIT_SUCCESS;
+  img_t bmap;
+  int thr;
+  double tstart, elapsed;
+
+  if ( argc != 2 ) {
+    fprintf(stderr, "Usage: %s thr < in.pgm > out.pgm\n", argv[0]);
+    return EXIT_FAILURE;
+  }
+  thr = atoi(argv[1]);
+  if (thr < 0 || thr >= 255) {
+    fprintf(stderr, "FATAL: invalid threshold %d\n", thr);
+    return EXIT_FAILURE;
+  }
+  read_pgm(stdin, &bmap);
+  if ( bmap.width % VLEN ) {
+    fprintf(stderr, "FATAL: the image width (%d) must be multiple of %d\n", bmap.width, (int)VLEN);
+    return EXIT_FAILURE;
+  }
+  tstart = hpc_gettime();
+  threshold(&bmap, thr);
+  elapsed = hpc_gettime() - tstart;
+  fprintf(stderr, "Executon time: %f\n", elapsed);
+  write_pgm(stdout, &bmap);
+  free_pgm(&bmap);
+  return EXIT_SUCCESS;
 }
+
+// vim: set nofoldenable :
